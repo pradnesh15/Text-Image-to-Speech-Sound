a9a1a203e3a7a64843eb581b158103bc5cfd1cc5
pradnesh15 <dpradynesh1504@gmail.com>
2024-02-12 22:10:45 +0530

    first commit

---
+++ ImageText_To_Sound.py
@@ -0,0 +1,34 @@
+import pytesseract
+from PIL import Image
+from gtts import gTTS
+import os
+
+
+def text_to_audio(text, output_file="output.mp3", lang="en"):
+    
+    tts = gTTS(text=text, lang=lang)
+    tts.save(output_file)
+    print(f"Audio saved as {output_file}")
+
+# Path to the Tesseract executable (may vary based on your system)
+pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+
+def read_text_from_image(image_path):
+    # Open the image file
+    with Image.open(image_path) as img:
+        # Use pytesseract to do OCR on the image
+        text = pytesseract.image_to_string(img)
+        return text
+
+if __name__ == "__main__":
+    # Path to the image file
+    image_path = 'C:\\Users\\HP\\Desktop\\STUDY material\\Projects\\img.png'
+
+    # Read text from the image
+    text = read_text_from_image(image_path)
+    
+    # Print the extracted text
+    print("Text extracted from image:")
+    print(text)
+    text_to_audio(text)
+
